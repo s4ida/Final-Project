fc629da78ed58e462bc9c28e4297aee3405e19d1
s4ida <sd88c9gvh@code.edu.az>
2024-02-05 04:09:37 +0400

    new changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,33 @@
-let animatedNumber = 0;
+const ourproducts = document.getElementById ('ourproducts')
 
-function updateNumber() {
-    // a güncelle
-    document.getElementById('animatedNumber').textContent = animatedNumber;
-
-    // Sayıyı hızla artır
-    animatedNumber += 1;
-
-    // Eğer sayı 40'a ulaştıysa sıfırla
-    if (animatedNumber > 40) {
-        animatedNumber = 0;
-    }
+function getproducts () {
+    page = 1
+    limit = 8
+     axios.get(`https://655c2fe4ab37729791aa011f.mockapi.io/swp102/products?page=${page}&limit=${limit}`)
+    .then(res=>{
+        products = res.data
+       products.map(item=>{
+            let box = document.createElement('div')
+            box.className = 'box col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3'
+        box.innerHTML = `
+        <div class="boxproducts">
+        <img src="${item.image}" alt="">
+        <h1>${item.Name}</h1>
+        <div class="detail">
+        <p>${item.Year}</p>
+        <p>${item.Mileage}KM</p>
+        <p>${item.FuelType}</p>
+</div>
+<p>Current Bid:<span>${item.CurrentBid}</span></p>
+        <button onclick="openProductDetail(${item.id})">View Details</button>
+        </div>
+        `
+        ourproducts.appendChild(box)
+        })
+        page++
+    })
 }
-
-// Sayfa her kaydırıldığında updateNumber fonksiyonunu çağır
-document.addEventListener('scroll', updateNumber);
-
-// İlk yüklenme sırasında bir kez çağır
-updateNumber();
+function openProductDetail(productId) {
+    window.location.href = `detailpage${productId}.html`;
+}
+getproducts()
