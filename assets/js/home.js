bae3630e856f5f41e635c639830800ea2bd6d3cf
s4ida <sd88c9gvh@code.edu.az>
2024-02-08 06:02:51 +0400

    some new change

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,7 @@
 const ourproducts = document.getElementById ('ourproducts')
 // const pagination = document.getElementById ('pagination')
 function getproducts () {
-   
+
      axios.get(`https://655c2fe4ab37729791aa011f.mockapi.io/swp102/products`)
     .then(res=>{
         products = res.data
@@ -12,24 +12,57 @@ function getproducts () {
         <div class="boxproducts">
         <img src="${item.image}" alt="">
         <h1>${item.Name}</h1>
-        <div class="detail">
-        <p>${item.Year}</p>
-        <p>${item.Mileage}KM</p>
-        <p>${item.FuelType}</p>
+        <button onclick="addtodetailpage(${item.id})"><a href="/detailpage.html">View Details</a>
+        <div class="buttons">
+    <button onclick="addtobasket(${item.id})"><i class="fa-solid fa-cart-shopping"></i></button>
+<button onclick="addtowishlist(${item.id})"><i class="fa-regular fa-heart"></i></button>
 </div>
-<p>Current Bid:<span>${item.CurrentBid}</span></p>
-        <button onclick="openProductDetail(${item.id})">View Details</button>
-        </div>
+</div>
         `
         ourproducts.appendChild(box)
         })
       
     })
 }
-function openProductDetail(productId) {
-    window.location.href = `detailpage${productId}.html`;
+function addtodetailpage(id) {
+  let detailpage = JSON.parse(localStorage.getItem('detailpage')) || [];
+      detailpage.push(products.find(item => item.id == id));
+  const maxDetailPageItems = 1;
+  if (detailpage.length > maxDetailPageItems) {
+      detailpage.shift();
+  }
+  localStorage.setItem('detailpage', JSON.stringify(detailpage));
 }
 getproducts()
+
+function addtobasket(id){
+  let cart = JSON.parse(localStorage.getItem('cart')) || []
+  let productitem = cart.find((item)=>item.id == id)
+  if(productitem){
+      productitem.count=(productitem.count || 1) +1
+  }
+  else{
+      let newitem = {...products.find(item=>item.id == id),count:1}
+      cart.push(newitem)
+  }
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
+
+function addtowishlist(id){
+  let wishlist= JSON.parse(localStorage.getItem('wishlist')) || []
+  let productitem = wishlist.find((item)=>item.id == id)
+  if(productitem){
+    alert('Bu mehsul daha evvel favorilere elave olunub')
+  }
+  else{
+      wishlist.push(products.find(item=>item.id == id))
+      localStorage.setItem('wishlist',JSON.stringify(wishlist))
+
+  }
+
+}
+
+
 
 const searchinput = document.getElementById('searchinput');
 const searchform = document.getElementById('searchform');
